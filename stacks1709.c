01a9ee3657f20330a836534784f0b8b5346a6bb3
John <53620374+pranay098@users.noreply.github.com>
2021-10-17 12:58:14 +0530

    Add files via upload

---
+++ stacks1709.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 20
+
+struct ArrStack
+{
+    int first;
+    int n;
+    int *arr;
+};
+
+
+struct ArrStack *Create(){
+    struct ArrStack *s=malloc(sizeof(struct ArrStack));
+    if (!s) return NULL;
+    s->n=MAXSIZE;
+    s->first=-1;
+    s->arr=malloc(s->n *sizeof(int));
+    if(!s->arr) return NULL;
+    return s;
+}
+
+int isEmpty(struct ArrStack *s){
+    return (s->first==-1);
+}
+/*
+if (s->first==1-) return 1;
+else return*/
+
+int isFull(struct ArrStack *s){
+    return (s->first == s->n - 1);
+}
+
+void push(struct ArrStack *s, int val ){
+    if (isFull(s)) printf("Stack is full");
+    else {
+        s->arr[++s->first]=val;
+        printf("%d pushed to stack\n", val);
+    }
+}
+
+int pop(struct ArrStack *s){
+    if (isEmpty(s)) {
+        printf("Stack is Empty");
+        return -1;
+    }
+    else {
+        return (s->arr[s->first--]);
+    }
+
+}
+
+int peek(struct ArrStack* s)
+{
+	if (isEmpty(s))
+		return -1;
+	return s->arr[s->first];
+}
+
+int count(struct ArrStack *s)
+{
+    
+    return s->first+1;
+}
+
+
+
+int main(){
+    struct ArrStack* s = Create(MAXSIZE);
+    push(s,4);
+    push(s,9);
+    push(s,7);
+    count(s);
+    printf("%d",peek(s));
+    //pop(s);
+    //printf("%d",isFull(s));
+    printf("\n%d",count(s));
+   
+    return 0;
+}
